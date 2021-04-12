df51a5ec7d13ce90420cd7bd1ae816bb4a35fd77
misskonstruction <misskonstruction@gmail.com>
2021-04-12 13:05:52 -0400

    update menu items to nav withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+
+const WoodenPlugsPage = () => (
+  <div>
+    <h1>Wooden Plugs Page</h1>
+  </div>
+  )
 
 function App() {
   return (
     <div>
-     <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path = '/woodenPlugs' component={WoodenPlugsPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,33 +11,38 @@ class Directory extends React.Component{
         {
           title: 'wooden plugs',
           imageUrl: 'https://i.etsystatic.com/10302514/r/il/190e26/2546950846/il_1140xN.2546950846_dbne.jpg',
-          id: 1  
+          id: 1,
+          linkUrl: 'woodenPlugs'  
         },
 
         {
           title: 'stainless steel plugs',
           imageUrl: 'https://cdn.shopify.com/s/files/1/0337/8189/products/SS-008-SS_087e3002-8df0-4478-ae1b-a2c19a912d58_360x.jpg?v=1612558616',
-          id: 2  
+          id: 2,
+          linkUrl: ''  
         },
 
         {
           title: 'acrylic plugs',
           imageUrl: 'https://i.pinimg.com/originals/be/67/d0/be67d04c3aa88549cd32bc3d97e07b0b.jpg',
-          id: 3 
+          id: 3,
+          linkUrl: ''  
         },
 
         {
           title: 'uniquely styled',
           imageUrl: 'https://i.pinimg.com/originals/0e/e8/0f/0ee80f65128598bdbbd325eadd59210d.jpg',
           size: 'large',
-          id: 4
+          id: 4,
+          linkUrl: '' 
         },
-        
+
         {
           title: 'stretching kits',
           imageUrl: 'https://cdn.shopify.com/s/files/1/0068/6312/products/starter_2000x.jpg?v=1549599896',
           size: 'large',
-          id: 5
+          id: 5,
+          linkUrl: '' 
          
         }
       ]
@@ -48,8 +53,8 @@ class Directory extends React.Component{
   	return(
   		<div className='directory-menu'>
   		{
-  			this.state.sections.map(({title, imageUrl, id, size})=> (
-  				<MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+  			this.state.sections.map(({id, ...otherSectionProps})=> (
+  				<MenuItem key={id} {...otherSectionProps}/>
   			))}
 
   		</div>
